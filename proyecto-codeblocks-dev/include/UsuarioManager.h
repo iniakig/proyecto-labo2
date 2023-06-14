ad74b65b4aeff4d14aed0fa8288d6523326f846b
Nahuel Alvez <nahue.alvez@gmail.com>
2023-06-14 11:00:13 -0300

    Se actualiza estado de realizacion de los metodos de UsuarioManager

--- proyecto-codeblocks-dev/include/UsuarioManager.h
+++ proyecto-codeblocks-dev/include/UsuarioManager.h
@@ -10,17 +10,17 @@ private:
     bool _permisos[3]; // Posición 0. Adm | 1. Sup | 2. Ven
 
     void listar(Usuario usuario, int tipoListado); // En curso
-    void listarPorAlias();
-    void listarPorDNI();
-    void listarActivos();
-    void listarInactivos();
+    void listarPorAlias(); // Hecho - Testear
+    void listarPorDNI(); // Hecho - Testear
+    void listarActivos(); // Hecho - Testear
+    void listarInactivos(); // Hecho - Testear
     void ordenarPorAlias(); // Pendiente
 
 public:
     void cargar(); // Hecho - Testear
-    void modificar(); // Pendiente
-    void darDeBaja(); // Pendiente
-    void reactivar(); // Pendiente
+    void modificar(); // Hecho - Testear
+    void darDeBaja(); // Hecho - Testear
+    void reactivar(); // Hecho - Testear
     void listarUsuarios(); // En curso
     void setPermisos(bool adm = false, bool sup = false, bool ven = false); // Hecho
 };
